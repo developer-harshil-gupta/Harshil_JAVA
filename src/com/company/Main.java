734705644b8645f4175bc8719747d3984f7c6d86
Harshil Gupta <67827826+developer-harshil-gupta@users.noreply.github.com>
2020-07-04 18:24:57 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print
-
-
-                ("Harshil Gupta");
+        System.out.print("Harshil Gupta");
         System.out.println("Harshil");
     }
 }
